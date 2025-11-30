d1e2beb2540d58056fa186a1167a0b8aff852a1e
Tomasz Waleciuk <Waleciuk+DataEnginnering@gmail.com>
2025-11-30 19:21:43 +0000

    logging utils module apparently ready

--- utils/logging_utils.py
+++ utils/logging_utils.py
@@ -3,7 +3,7 @@ import logging
 from file_system_utils import nth_level_parent_dir
 
 logger = logging.getLogger(__name__)
-
+NUMBER_OF_LEVELS_FROM_PROJECT_ROOT = 1
 
 def _ensure_log_directory(base_path=None):
     """Ensure the logs directory exists.
@@ -12,7 +12,7 @@ def _ensure_log_directory(base_path=None):
 
     # resolves project root, provided this module is contained
     # in a subdirectory, hence '1 level down'
-    project_root = nth_level_parent_dir(base_path, 1)
+    project_root = nth_level_parent_dir(base_path, NUMBER_OF_LEVELS_FROM_PROJECT_ROOT)
 
     # logs stored in 'logs' subfolder
     log_directory = project_root / 'logs'
@@ -27,6 +27,7 @@ def _ensure_log_directory(base_path=None):
     log_directory.mkdir(parents=True, exist_ok=True)
     return log_directory
 
+
 def _create_formatter():
     """Create a standard log formatter."""
     formatter = ' - '.join(
@@ -36,9 +37,10 @@ def _create_formatter():
         '%(name)s',
         '%(message)s',
         )
+    # returns a logging.Formatter in the above format
     return logging.Formatter(formatter)
-    
-    #######################################################################    
+
+    #######################################################################
     # "%(asctime)s | "
     # "%(levelname)-8s | "  # -8s pads the level name to 8 chars for alignment
     # "%(filename)s:%(lineno)d | "
@@ -46,42 +48,39 @@ def _create_formatter():
     # "%(message)s"
     # %(filename)s: filename portion of the path (e.g., main.py).
     # %(lineno)d: source line number where the call was issued (e.g., 42).
-    #######################################################################    
-
-# def _create_formatter():
-#     """Create a standard log formatter."""
-#     return logging.Formatter(
-#         '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-#     )
+    #######################################################################
 
 
-# def _create_handlers(log_directory, log_file, level):
-#     """Create file and console handlers."""
-#     file_handler = logging.FileHandler(log_directory / log_file)
-#     file_handler.setLevel(level)
 
-#     console_handler = logging.StreamHandler()
-#     console_handler.setLevel(level)
+def _create_handlers(log_directory, log_file, level):
+    """Create file and console handlers."""
+    file_handler = logging.FileHandler(log_directory / log_file)
+    file_handler.setLevel(level)
 
-#     formatter = _create_formatter()
-#     file_handler.setFormatter(formatter)
-#     console_handler.setFormatter(formatter)
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(level)
 
-#     return file_handler, console_handler
+    # receives the required formatter format
+    formatter = _create_formatter()
+    # applies the formatter to file and console handlers
+    file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
+
+    return file_handler, console_handler
 
 
-# def setup_logger(name, log_file, level=logging.DEBUG, base_path=None):
-#     """Function to setup a logger; can be used in multiple modules."""
-#     log_directory = _ensure_log_directory(base_path)
+def setup_logger(name, log_file, level=logging.DEBUG, base_path=None):
+    """Function to setup a logger; can be used in multiple modules."""
+    log_directory = _ensure_log_directory(base_path)
 
-#     logger = logging.getLogger(name)
-#     logger.setLevel(level)
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
 
-#     if not logger.handlers:
-#         file_handler, console_handler = _create_handlers(
-#             log_directory, log_file, level
-#         )
-#         logger.addHandler(file_handler)
-#         logger.addHandler(console_handler)
+    if not logger.handlers:
+        file_handler, console_handler = _create_handlers(
+            log_directory, log_file, level
+        )
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
 
-#     return logger
+    return logger
