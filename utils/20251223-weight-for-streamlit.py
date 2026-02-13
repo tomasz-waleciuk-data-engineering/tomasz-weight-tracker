78214f23badd6712c6afb844c7d747b3bbb0b5ba
Tomasz Waleciuk <Waleciuk+DataEnginnering@gmail.com>
2026-02-13 12:15:31 +0000

    Update 20251223-weight-for-streamlit.py
    auto BMI start as (minimun weight - 1)

--- utils/20251223-weight-for-streamlit.py
+++ utils/20251223-weight-for-streamlit.py
@@ -205,12 +205,6 @@ def bmi_to_kg_list(bmi_range, bmi_step, height):
 # 4. MAIN APP LOGIC
 # ==========================================
 
-h1 = 182
-h2 = h1  # + 1
-bmi_start = 25
-bmi_end = 27
-bmi_step = 0.1
-
 os_environ_hostname = os.environ.get('HOSTNAME', 'unknown-host')
 st.write('\'HOSTNAME\' if known: ', os_environ_hostname)
 
@@ -286,7 +280,16 @@ if not df.empty:
         if c in fig_01_df.columns:
             fig_01_df[c] = pd.to_numeric(fig_01_df[c], errors='coerce')
 
-    st.write(bmi_to_kg_list(range(bmi_start, bmi_end+1), bmi_step, h1))
+        
+    h1 = 182
+    h2 = h1  # + 1
+    # bmi_start = 25
+    # bmi_end = 26
+    bmi_end = int(fig_01_df['Weight'].min())
+    bmi_start = bmi_end - 1
+    bmi_step = 0.1
+    
+    st.write(bmi_to_kg_list(range(bmi_start, bmi_end+1), bmi_step, h1))    
     if h1 != h2:
         st.write(bmi_to_kg_list(range(bmi_start, bmi_end+1), bmi_step, h2))
     st.warning(f'All readings for {days_since} days')        
