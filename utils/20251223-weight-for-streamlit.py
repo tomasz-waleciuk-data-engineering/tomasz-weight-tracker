959941ccef40f836d06f2dd430858ee3feb20292
Tomasz Waleciuk <Waleciuk+DataEnginnering@gmail.com>
2026-03-01 13:58:13 +0000

    Update 20251223-weight-for-streamlit.py
    some morte debugging

--- utils/20251223-weight-for-streamlit.py
+++ utils/20251223-weight-for-streamlit.py
@@ -296,7 +296,7 @@ if not df.empty:
     start_date_mean = end_date_mean - pd.Timedelta(days=days_for_mean)
     my_bmi_base = fig_01_df.loc[start_date_mean:end_date_mean]['Weight'].mean()
     st.dataframe(fig_01_df.loc[start_date_mean:end_date_mean])
-    st.warning(my_bmi_base)
+    st.warning(my_bmi_base, days_for_mean, start_date_mean, end_date_mean)
     # my_bmi_base fig_01_df['Weight'].min()  # previously hostorical minimum weight was used for bmi base
     bmi_end = int(my_bmi_base / ((h1/100)**2))
     bmi_start = bmi_end - 1
