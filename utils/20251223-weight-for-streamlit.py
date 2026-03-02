a35f4586262d76aa24064b94608c0c256d099d34
Tomasz Waleciuk <Waleciuk+DataEnginnering@gmail.com>
2026-03-02 21:46:45 +0000

    Update 20251223-weight-for-streamlit.py
    start date to str for metrics

--- utils/20251223-weight-for-streamlit.py
+++ utils/20251223-weight-for-streamlit.py
@@ -368,7 +368,7 @@ if not df.empty:
 
         col1, col2, col3, col4 = st.columns(4)
         with col1:
-            st.metric('start date', start_date)
+            st.metric('start date', str(start_date))
         with col2:
             st.metric('max weight', max_weight)
         with col3:
